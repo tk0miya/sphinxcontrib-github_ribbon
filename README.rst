5f930d7cae5cbfe0294c1d3f616f5e01b3dc8364
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-11-24 12:49:11 +0900

    Initial commit

---
+++ README.rst
@@ -0,0 +1,25 @@
+sphinxcontrib-githubribbon
+==========================
+
+`sphinxcontrib-githubribbon` is a Sphinx extension to put a `GitHub Ribbon` to each HTML pages.
+
+.. _GitHub Ribbon: https://github.com/blog/273-github-ribbons
+
+Usage
+-----
+
+Append this extension in conf.py::
+
+    extensions = ['sphinxcontrib.github_ribbon']
+
+
+And update settings:
+
+``github_ribbon_repo``
+    The location of your repository.  By default, `"sphinx-doc/sphinx"`.
+
+``github_ribbon_position`` = ("left" | "right")
+    The position of ribbon.  By default, `"right"`.
+
+``github_ribbon_color`` = ("red" | "green" | "darkblue" | "orange" | "gray" | "white")
+    The color of ribbont.  By default, `"red"`.
---
+++ sphinxcontrib/github_ribbon.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+    sphinxcontrib.githubribbon
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: Copyright 2016 by Takeshi KOMIYA <i.tkomiya@gmail.com>
+    :license: BSD, see LICENSE for details.
+"""
+
+from docutils import nodes
+
+
+GITHUB_RIBBON_IMAGE = {
+    ('left', 'red'): '<img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png">',
+    ('left', 'green'): '<img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png">',
+    ('left', 'darkblue'): '<img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png">',
+    ('left', 'orange'): '<img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/8b6b8ccc6da3aa5722903da7b58eb5ab1081adee/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png">',
+    ('left', 'gray'): '<img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/c6625ac1f3ee0a12250227cf83ce904423abf351/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png">',
+    ('right', 'white'): '<img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/c6286ade715e9bea433b4705870de482a654f78a/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_white_ffffff.png">',
+    ('right', 'red'): '<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png">',
+    ('right', 'green'): '<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png">',
+    ('right', 'darkblue'): '<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png">',
+    ('right', 'orange'): '<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png">',
+    ('right', 'gray'): '<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png">',
+    ('right', 'white'): '<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png">',
+}
+
+
+def get_ribbon_tag(config):
+    url = 'https://github.com/%s' % config.github_ribbon_repo
+    key = (config.github_ribbon_position, config.github_ribbon_color)
+    return '<a href="%s">%s</a>' % (url, GITHUB_RIBBON_IMAGE.get(key, ''))
+
+
+def on_doctree_resolved(app, doctree, docname):
+    ribbon = nodes.raw('', get_ribbon_tag(app.config), format='html')
+    doctree.insert(0, ribbon)
+
+
+def setup(app):
+    app.connect('doctree-resolved', on_doctree_resolved)
+    app.add_config_value('github_ribbon_repo', 'sphinx-doc/sphinx', 'html')
+    app.add_config_value('github_ribbon_position', 'right', 'html')
+    app.add_config_value('github_ribbon_color', 'red', 'html')
